340513cc5e603050a36571bdf8ecf8d6ff1a05c4
saoban <wakilabdulazeez@gmail.com>
2023-09-04 16:44:07 +0100

    File created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="m-auto bg-gray-900">
+      <p className="text-center text-xl text-yellow-500 ">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
