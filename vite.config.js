02c006bf7aa5d4cddb4b18b1cc2b0a50b600d923
harzeezco <wakilabdulazeez@gmail.com>
2023-10-18 16:55:16 +0100

    remove a depedency causing deployment issue

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ViteSW } from 'vite-plugin-sw';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    ViteSW({
-      runtimeCaching: [
-        {
-          urlPattern: ({ url }) => {
-            return url.pathname.startsWith('/');
-          },
-          handler: 'CacheFirst',
-        },
-      ],
-    }),],
+  plugins: [react()],
 
 })
